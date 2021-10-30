b0c81179ca1e8d4f92557f94331bca3cdb4c6b25
LENOVO <souha.sghaier@esprit.tn>
2021-10-30 15:02:59 +0100

    Third commit

---
+++ Desktop/gestiodesemployees/connection.cpp
@@ -0,0 +1,34 @@
+#include "connection.h"
+
+Connection::Connection()
+{
+
+}
+
+bool Connection::createconnect()
+{bool test=false;
+QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+
+db.setDatabaseName("test-db");//inserer le nom de la source de données ODBC
+db.setUserName("system");//inserer nom de l'utilisateur
+db.setPassword("souha2021");//inserer mot de passe de cet utilisateur
+
+if (db.open())
+return true;
+
+
+
+
+
+    return  test;
+}
+
+bool Connection::ouvrirConnexion()
+{
+
+  if (db.open())
+      return true;
+  else
+      return false;
+}
+void Connection::fermerConnection(){db.close();}
--- Desktop/gestiodesemployees/main.cpp
+++ Desktop/gestiodesemployees/main.cpp
@@ -1,11 +1,21 @@
  #include "mainwindow.h"
-
+#include "connection.h"
 #include <QApplication>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    Connection c;
+    c.createconnect();
     MainWindow w;
+    bool test;
+    test=c.ouvrirConnexion();
+    if (test ==true )
+        QMessageBox::information(nullptr,"Data base","Connection succeful");
+    else
+        QMessageBox::warning(nullptr,"Data base","failed");
     w.show();
+
     return a.exec();
 }
